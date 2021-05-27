af7002fb0952453abcdb82ad1eb01fcb143fec48
tal7aouy <talhaouycoding@gmail.com>
2021-05-27 13:38:09 +0100

    create navabar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="text-center bg-indigo-600 text-white p-4 mb-11">
+    <div class="text-xl md:text-4xl font-semibold mb-3">
+      <i class="fas fa-virus text-green-400"></i><i class="fas fa-virus text-green-400"></i> Covid-19 Tracker
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
